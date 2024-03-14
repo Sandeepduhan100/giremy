a8cf964e144dda6b75326164d27c250620b85142
unknown <sandeepduhan1432@gmail.com>
2024-03-13 22:02:13 -0700

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode:false,
-    images:{
-        domains:['media.graphassets.com']
-    }, experimental: {
+    reactStrictMode: false,
+    images: {
+        // Use an array of objects to specify remote patterns
+        remotePatterns: [
+            {
+                // Use the 'hostname' key instead of 'host'
+                hostname: 'media.graphassets.com'
+            }
+        ]
+    },
+    experimental: {
         missingSuspenseWithCSRBailout: false,
-      },
+    },
 };
 
 export default nextConfig;
